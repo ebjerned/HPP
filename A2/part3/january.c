23e151933c28a847c1965f7b1e3fcca8169f0c1e
Erik Bjerned <erbj0497@vitsippa.it.uu.se>
2023-01-25 16:16:42 +0100

    A2 Task 3

--- A2/part3/january.c
+++ A2/part3/january.c
@@ -3,7 +3,7 @@
 
 
 typedef struct temprange {
-	int* next_node;
+	struct temprange* next_node;
 	int index;
 	float min, max;
 } temprange_node;
@@ -16,7 +16,7 @@ void print_database(temprange_node* db);
 
 
 int main(){
-	char a;
+	char comm;
 	int index;
 	float min, max;
 	int running = 1;
@@ -25,17 +25,16 @@ int main(){
 
 	while(running){
 		printf("Enter command: ");
-		scanf(" %c",&a);
-		switch(a){
+		scanf(" %c",&comm);
+		switch(comm){
 			case 'A':
-				scanf(" %i %f %f",&index, &min, &max); 
-				printf("Index: %i%f%f \n", index,min,max);
+				scanf(" %i %f %f", &index, &min, &max); 
 				add_to_database(index, min, max, &root);
-				printf("%p\n", root);
 				break;
 			case 'D':
 				scanf(" %i", &index);
 				printf("Deleted: %i \n", index);
+				remove_from_database(index, &root);
 				break;
 			case 'P':
 				print_database(root);
@@ -44,8 +43,9 @@ int main(){
 				running = 0;
 				break;
 			default:
-				continue;
+				printf("Command not defined\n");
 		}
+		printf("\n");
 	}
 	remove_database(&root);
 	return 0;
@@ -55,49 +55,51 @@ int main(){
 int add_to_database(int index, float min, float max, temprange_node** root){
 	if (index > 31 || index < 1) return 0;
 	temprange_node* new_entry = (temprange_node*)malloc(sizeof(temprange_node));
-	
+
 	new_entry->index = index;
 	new_entry->min = min;
 	new_entry->max = max;
+	new_entry->next_node = NULL;
+
 	if(*root == NULL){
+		printf("Null root");
 		*root = new_entry;
-		printf("Created rootnode\n");
-		printf("%p\n", root);
 		return 1;
 	}
 	temprange_node* curr = *root;
-	while(curr != NULL){
-		printf("Looping");
-		if (curr->index > index){
-			new_entry->next_node = (int*)(*root);
-			*root = new_entry;
-			return 1;
-		}
-		temprange_node* next = (temprange_node*)(curr->next_node);
-		if(next != NULL){
-			if(next->index > index && curr->index < index){
-				new_entry->next_node = curr->next_node;
-				curr->next_node = (int*)new_entry;
-				return 1;
-			} 
-			if(next->index == index){
-				new_entry->next_node = next->next_node;
-				curr->next_node = (int*)new_entry;
-				free(next);
-				return 1;	
-			}
+	if(curr->index > index){
+		printf("Smaller than root");
+		new_entry->next_node = *root;
+		*root = new_entry;
+		return 1;
+	}
+	if(curr->index == index){
+		if(curr->next_node != NULL) new_entry->next_node = curr->next_node;
+		free(curr);
+		*root = new_entry;
+		return 1;
+	}
+	while(curr->next_node != NULL && curr->next_node->index < index){
 
-		} else {
-			curr->next_node = (int*)new_entry;
-			return 1;
-		}
-//		printf("%p", curr);
-		curr = next;
-//		printf("%p", curr);
-//		break;
+		curr = curr->next_node;
 
 	}
 
+	if(curr->next_node == NULL){
+		curr->next_node = new_entry;
+		return 1;
+	}else if (curr->next_node->index > index){
+		new_entry->next_node = curr->next_node;
+		curr->next_node = new_entry;
+		return 1;
+	}else if (curr->next_node->index == index){
+		new_entry->next_node = curr->next_node->next_node;
+		free(curr->next_node);
+		curr->next_node = new_entry;
+		return 1;
+	}
+	
+	return 0;
 }
 
 void print_database(temprange_node* root){ 
@@ -117,10 +119,48 @@ void print_database(temprange_node* root){
 	return;
 }
 
+int remove_from_database(int index, temprange_node** root){
+	if (*root == NULL) return 0;
+
+	temprange_node* curr = *root;
+	if ((*root)->index == index){
+		if((*root)->next_node == NULL) {
+			free(*root);
+			*root = NULL;
+			return 1;
+		} else  {
+			temprange_node* next = (*root)->next_node;
+			free(*root);
+			*root = next;
+			return 1;
+		}
+		return 1;
+	}
+
+	while(curr->next_node != NULL && curr->next_node->index != index){
+		curr = curr->next_node;
+	}
+	if(curr->next_node == NULL) return 0;
+
+	if(curr->next_node->index == index && curr->next_node != NULL){
+		if(curr->next_node->next_node != NULL){
+			temprange_node* next = curr->next_node->next_node;
+			free(curr->next_node);
+			curr->next_node = next;
+		}else {
+			free(curr->next_node);
+			curr->next_node = NULL;
+
+		}
+	} else {
+		return 0;
+	}
+	return 0;
+}
+
 int remove_database(temprange_node** root){
 	temprange_node* curr = *root;
 	while(curr != NULL){
-		
 		if(curr->next_node == NULL){
 			free(curr);
 			return 1;
@@ -130,5 +170,5 @@ int remove_database(temprange_node** root){
 		curr = next;
 
 	}
-
+	return 1;
 }

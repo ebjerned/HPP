5eb429b45c5df2347a02d53c696fc9d7da31e8f9
Erik Bjerned <erbj0497@vitsippa.it.uu.se>
2023-02-14 10:47:47 +0100

    A3, plotting

--- A3/plotting.py
+++ A3/plotting.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
 N = [10, 50, 100, 500, 1000, 5000, 10000]
-N_2 = [e**2 for e in range(140)]
-t = range(140)
+N_2 = [e**2 for e in range(10000)]
+t = range(10000)
 time = [0.002901, 0.006463, 0.015964, 0.325657, 1.295745, 32.431196, 130.429485]
 
 plt.loglog(N_2, t)

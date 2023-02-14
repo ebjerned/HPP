771a9c566b280a426e0e720173723f6fc3d79273
Erik Bjerned <erbj0497@vitsippa.it.uu.se>
2023-02-14 10:45:07 +0100

    A3, plotting

---
+++ A3/plotting.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+N = [10, 50, 100, 500, 1000, 5000, 10000]
+N_2 = [e**2 for e in range(140)]
+t = range(140)
+time = [0.002901, 0.006463, 0.015964, 0.325657, 1.295745, 32.431196, 130.429485]
+
+plt.loglog(N_2, t)
+plt.loglog(N, time)
+plt.legend(["N^2", "Measured time"])
+plt.title("Time complexity")
+plt.xlabel("Particles [-]")
+plt.ylabel("Time [s]")
+plt.grid()
+plt.savefig("time_complexity.png")

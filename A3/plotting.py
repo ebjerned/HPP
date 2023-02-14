4456bb7a75dd4cf24c284aad8920b8e01f8843ec
Erik Bjerned <erbj0497@vitsippa.it.uu.se>
2023-02-14 10:55:47 +0100

    A3, plotting

--- A3/plotting.py
+++ A3/plotting.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 
 N = [10, 50, 100, 500, 1000, 5000, 10000]
 N_2 = [e**2 for e in range(1, 10000, 100)]
-t = range(1, 10000, 100)
 time = [0.002901, 0.006463, 0.015964, 0.325657, 1.295745, 32.431196, 130.429485]
 
-#plt.loglog(N_2, t)
+
+plt.loglog(N_2, N_2)
 plt.loglog(N, time)
 plt.legend(["N^2", "Measured time"])
 plt.title("Time complexity")

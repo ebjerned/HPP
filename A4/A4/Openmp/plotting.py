815260bbb5cca50d74fb3cd0a6a9cfaa9f30fe55
Erik Bjerned <erbj0497@vitsippa.it.uu.se>
2023-02-20 14:18:22 +0100

    A4, plotting omp

--- A4/A4/Openmp/plotting.py
+++ A4/A4/Openmp/plotting.py
@@ -4,10 +4,11 @@ import numpy as np
 t= np.loadtxt("speedup.txt")
 
 threads = [e[0] for e in t]
-s = [e[1]/t[0][1] for e in t]
+s = [t[0][1]/e[1] for e in t]
 
 
-plt.plot(threads,s)
+plt.plot(threads,s, "ro")
+plt.plot(threads, threads)
 plt.xlabel("Number of threads")
 plt.ylabel("Performance increase")
 plt.savefig("openmp_speedup")

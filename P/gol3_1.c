5d6903c1b1ba312254a66a38a0c5c137708be2fa
Erik Bjerned <erbj0497@tussilago.it.uu.se>
2023-03-10 17:59:30 +0100

    P, contigous or not does not seem to matter at this size or unparrallelized

--- P/gol3_1.c
+++ P/gol3_1.c
@@ -16,7 +16,7 @@ void printVolume(char*** volume, int x, int y, int z);
 int** findAlive(char*** volume, int x, int y, int z);
 int** findAlive2(char* volume, int x, int y, int z);
 
-void processNeighbours(int** dest, char*** neigh_count, int** all, int x, int y, int z, char*** test_byte);
+void processNeighbours(int** dest, int** all, int x, int y, int z, char*** test_byte);
 void processNeighbours2(int** dest, int** all, int x, int y, int z, char* test_byte);
 double get_wall_seconds();
 
@@ -30,14 +30,14 @@ int main(int argc, char* argv[]){
 	int size = 35;
 
 	int** updated = generateCoordinateArrays(size, size, size);
-	char* test_byte = generateContigousVolume(35,35,35);
-	char* new_test_byte = generateContigousZeroVolume(size,size,size);
+	char*** test_byte = generateVolume(35,35,35);
+	char*** new_test_byte = generateZeroVolume(size,size,size);
 //	printf("Initial state: \n");
 //	printVolume(states, size, size, size);
-	int** alive = findAlive2(test_byte,size,size,size);
+	int** alive = findAlive(test_byte,size,size,size);
 
 	int** all = generateCoordinateArrays(size*3, size*3, size*3);
-	processNeighbours2(all, alive, size,size,size, test_byte);
+	processNeighbours(all, alive, size,size,size, test_byte);
 
 	printf("Setup complete\n\n");
 	while(1){
@@ -48,55 +48,51 @@ int main(int argc, char* argv[]){
 		char cell, count;
 //		printVolume(neigh_count, size, size, size);
 //		printVolume(states, size, size, size);
-		for(r = 0; r < all[0][0]; r++){
+		int limit = all[0][0];
+		for(r = 0; r < limit; r++){
 			i = all[1][r];
 			j = all[2][r];
 			k = all[3][r];
 //			int count = neigh_count[i][j][k];
 //			int index = i*x*y + j*x + i;
-			index = i*size*size + j*size + k;
-			cell = test_byte[index];
-
+			//index = (i*size + j)*size + k;
+//			cell = test_byte[index];
+			cell = test_byte[i][j][k];
 			if(cell&1){
 				count = (cell & 62) >> 1;
 
 				if(count != 5 && count != 6 && count !=7){
-//					new_states[i][j][k] = 0;
 					updated[1][update_count] = i;
                                         updated[2][update_count] = j;
                                         updated[3][update_count] = k;
                                         update_count++;
-					new_test_byte[index] = 0;
+//					new_test_byte[index] = 0;
+					new_test_byte[i][j][k] = 0;
 				}
 			} else {
 				count = (cell & 62) >> 1;
 
 				if(count==6){
-	//				new_states[i][j][k] = 1;
 					updated[1][update_count] = i;
 					updated[2][update_count] = j;
 					updated[3][update_count] = k;
 					update_count++;
-					new_test_byte[index] = 1;
+//					new_test_byte[index] = 1;
+					new_test_byte[i][j][k] = 1;
 				}
 			}
-//			neigh_count[i][j][k] = 0;
-//			char state = test_byte[index] & 1;
-			test_byte[index] &= ~62;
-//			assert(((test_byte[index] & 62) >>1 ) == 0);
-//			assert(states[i][j][k] == state);
-//			assert(new_states[i][j][k] == (new_test_byte[index] & 1));
-		
+			test_byte[i][j][k] &= ~62;
+
 		}
 
 		updated[0][0] = update_count;
-		processNeighbours2(all, updated, size, size, size, test_byte);
+		processNeighbours(all, updated, size, size, size, test_byte);
 
 		memcpy(test_byte, new_test_byte, size*size*size*sizeof(char));
-
 		printf("Generation done in %lf s\n\n", get_wall_seconds()-time);
 
 		incr++;
+
 		if (incr == 2){
 			printf("Time steps maxed out\n");
 			break;
@@ -280,7 +276,7 @@ int** findAlive2(char* volume, int x, int y, int z){
 }
 
 // TODO: When loopin through neighbours, count how many times a neighbour is mentioned. This can replace the neighbour checking in the main loop
-void processNeighbours(int** dest, char*** neigh_count, int** alive,  int x, int y, int z, char*** test_byte){
+void processNeighbours(int** dest, int** alive,  int x, int y, int z, char*** test_byte){
 	double time = get_wall_seconds();
 	int** all = dest;
 
@@ -289,19 +285,25 @@ void processNeighbours(int** dest, char*** neigh_count, int** alive,  int x, int
 
 
 	int limit = alive[0][0];
+
+	char l_l,l,l_u,m_l,m,m_u,n_l,n,n_u;
+	char* cell;
+	int* z_axis = alive[1];
+	int* y_axis = alive[2];
+	int* x_axis = alive[3];
 	for(int i = 0; i < limit; i++){
-		int z_coord = alive[1][i];
-		int y_coord = alive[2][i];
-		int x_coord = alive[3][i];
+		int z_coord = z_axis[i];
+		int y_coord = y_axis[i];
+		int x_coord = x_axis[i];
 	
 
 
-		char l_u = 2;
-		char l_l = -1;
-		char m_u = 2;
-		char m_l = -1;
-		char n_u = 2;
-		char n_l = -1;
+		l_u = 2;
+		l_l = -1;
+		m_u = 2;
+		m_l = -1;
+		n_u = 2;
+		n_l = -1;
 
 		if(!z_coord){
 			l_l = 0;
@@ -319,26 +321,27 @@ void processNeighbours(int** dest, char*** neigh_count, int** alive,  int x, int
 			n_u = 1;
 		}
 
-		for(char l = l_l; l < l_u; l++){
-                        for(char m = m_l; m < m_u; m++){
-                                for(char n = n_l; n < n_u; n++){
+		for(l = l_l; l < l_u; l++){
+                        for(m = m_l; m < m_u; m++){
+                                for(n = n_l; n < n_u; n++){
 //					double temp = get_wall_seconds();
 
-					char* neighbour = &(neigh_count[z_coord+l][y_coord+m][x_coord+n]);
-					char* cell = &(test_byte[z_coord+l][y_coord+m][x_coord+n]);
+					cell = &(test_byte[z_coord+l][y_coord+m][x_coord+n]);
 
-					if(!(*neighbour)){
+//					assert(!(*neighbour) == (!((*cell)&62)));
+
+					if(!((*cell)&62)){
 						all[1][counter] = z_coord+l;
 						all[2][counter] = y_coord+m;
 						all[3][counter] = x_coord+n;
 						counter++;
 					}
-					*neighbour += 1;
 
-					char state = (*cell) & 1;
-					char count = (((*cell) & 62) >> 1) + 1;
-					*cell = (count << 1) + state;
-//					timecon += get_wall_seconds()-temp;
+//					char state = (*cell) & 1;
+//					char count = (((*cell) & 62) >> 1) + 1;
+//					*cell = (count << 1) + state;
+					*cell = (((((*cell) & 62) >> 1) + 1 ) << 1) + ((*cell) & 1);
+
 
 				}
 			}
